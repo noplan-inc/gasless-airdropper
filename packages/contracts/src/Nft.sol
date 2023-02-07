ef979cc9ebd8ea9e7e2479a7e3da22b8b109e4a3
yukiorita1117 <3204704@gmail.com>
2023-02-08 00:25:11 +0900

    feat nft

--- packages/contracts/src/Nft.sol
+++ packages/contracts/src/Nft.sol
@@ -3,18 +3,27 @@ pragma solidity ^0.8.0;
 
 import "openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "./interfaces/INFT.sol";
 
-
-
-contract NftDemo is ERC721, INFT {
+contract Nft is ERC721, INFT {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
     constructor()ERC721("NftDemo", "NFT"){}
 
-    function mint(address to) public{
+    function safeMint(address to, string memory uri) public{
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
