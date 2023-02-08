84d4e913b3675c13d2b67ea6caed6bc67b3b8ba6
janken58 <np.kobayashi58@gmail.com>
2023-02-08 22:24:13 +0900

    fix _document.tsx

--- packages/frontend/pages/home/index.tsx
+++ packages/frontend/pages/home/index.tsx
@@ -92,13 +92,6 @@ function connectWallet() {
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>gasless airdoropper</title>
-        <meta name="description" content="gasless airdoropper application" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* TODO: add favicon */}
-        {/* <link rel="icon" href="/" /> */}
-      </Head>
       <main>
         <Header />
         {connectWallet()}
--- packages/frontend/pages/list/index.tsx
+++ packages/frontend/pages/list/index.tsx
@@ -5,13 +5,6 @@ import { Header } from "@/components/header";
 const List: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>gasless airdoropper</title>
-        <meta name="description" content="gasless airdoropper application" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* TODO: add favicon */}
-        {/* <link rel="icon" href="/" /> */}
-      </Head>
       <main>
         <Header />
         MintされたToken一覧
