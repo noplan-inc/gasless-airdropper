3644ab96d073941cccc4b3cd7f683002ab616c19
somasekimoto <0421.soma@gmail.com>
2023-02-09 00:29:06 +0900

    WIP: sendPKPTransaction の override やけど invalid Signatureでエラー祭り

--- packages/lit-sandbox-front/src/App.tsx
+++ packages/lit-sandbox-front/src/App.tsx
@@ -2,13 +2,145 @@ import React, { useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
 // @ts-ignore
-import LitJsSdk from "@lit-protocol/sdk-browser";
+import LitJsSdk, { checkAndSignAuthMessage } from "@lit-protocol/sdk-browser";
 import { providers, Contract } from 'ethers';
 import { parseEther, formatBytes32String } from 'ethers/lib/utils';
 
 import erc20ABI from "./erc20.json";
 import airdropABI  from "./airdrop-abi.json"
+import sign from "./sign.json"
 
+class LitClient extends LitJsSdk.LitNodeClient {
+  // let ready;
+  constructor(config:any){
+    super(config)
+  }
+
+  async connect(){
+    await super.connect()
+  }
+
+  async sendPKPTransaction({
+    provider,
+    to,
+    value,
+    data,
+    chain,
+    publicKey,
+    gasPrice,
+    gasLimit,
+  }: any) {
+    const signResult = await this.signPKPTransaction({
+      to,
+      value,
+      data,
+      gasPrice,
+      gasLimit,
+      chain,
+      publicKey,
+    });
+
+    const tx = signResult.response;
+    const signature = signResult.signatures["sig1"].signature;
+    const serializedTx = super.serialize(tx, signature);
+    return provider.sendTransaction(serializedTx);
+  }
+
+  async signPKPTransaction({
+    to,
+    value,
+    data,
+    chain,
+    publicKey,
+    gasPrice,
+    gasLimit,
+  }: any) {
+    // if (!super.ready) {
+    //   super.throwError({
+    //     message:
+    //       "LitNodeClient is not ready.  Please call await litNodeClient.connect() first.",
+    //     name: "LitNodeClientNotReadyError",
+    //     errorCode: "lit_node_client_not_ready",
+    //   });
+    // }
+    console.log(super.LIT_CHAINS)
+    console.log(super.LIT_CHAINS)
+
+    // const chainId = await super.LIT_CHAINS[chain].chainId;
+    const chainId = 80001;
+    // if (!chainId) {
+    //   super.throwError({
+    //     message: "Invalid chain.  Please pass a valid chain.",
+    //     name: "InvalidChain",
+    //     errorCode: "invalid_input_chain",
+    //   });
+    // }
+
+    // if (!publicKey) {
+    //   super.throwError({
+    //     message: "Pubic Key not provided.  Please pass a valid Public Key.",
+    //     name: "MissingPublicKey",
+    //     errorCode: "missing_public_key",
+    //   });
+    // }
+
+    // const authSig = await checkAndSignAuthMessage({ 
+    //   to: to,
+    //   value: value,
+    //   data: data,
+    //   publicKey: publicKey,
+    //   gasPrice: gasPrice,
+    //   gasLimit: gasLimit,
+    //   chain: chain,
+    //   expiration: new Date(Date.now() + 24 * 60 * 60 * 500).toISOString() 
+    // });
+    // console.log("authSig")
+    // console.log(authSig)
+    // return;
+
+    const authSig = sign
+
+    const signLitTransaction = `
+      (async () => {
+        const fromAddress = ethers.utils.computeAddress(publicKey);
+        const latestNonce = await LitActions.getLatestNonce({ address: fromAddress, chain });
+        const txParams = {
+          nonce: latestNonce,
+          gasPrice,
+          gasLimit,
+          to,
+          value,
+          chainId,
+          data,
+        };
+        LitActions.setResponse({ response: JSON.stringify(txParams) });
+        
+        const serializedTx = ethers.utils.serializeTransaction(txParams);
+        const rlpEncodedTxn = ethers.utils.arrayify(serializedTx);
+        const unsignedTxn =  ethers.utils.arrayify(ethers.utils.keccak256(rlpEncodedTxn));
+        const sigShare = await LitActions.signEcdsa({ toSign: unsignedTxn, publicKey, sigName });
+      })();
+    `;
+
+    return await super.executeJs({
+      code: signLitTransaction,
+      authSig,
+      jsParams: {
+        publicKey,
+        chain,
+        sigName: "sig1",
+        chainId,
+        to,
+        value,
+        data,
+        gasPrice: gasPrice || "0x2e90edd000",
+        gasLimit: gasLimit || "0x" + (30000).toString(16),
+      },
+    });
+  }
+
+
+}
 
 const ConnectButton: React.FC<{}> = () => {
   const sendMaticHandler = useCallback(async () => {
@@ -64,9 +196,27 @@ const ConnectButton: React.FC<{}> = () => {
   }, []);
 
   const airdropNftHandler = useCallback(async () => {
-    const client = new LitJsSdk.LitNodeClient({ litNetwork: "serrano" });
+    const client = new LitClient({ litNetwork: "serrano" });
     await client.connect();
     console.log(client);
+    console.log("client.authSig")
+    // console.log(await client.authSig)
+    // let auth = checkAndSignAuthMessage()
+    console.log("auth")
+    // client.signPKPTransaction(
+    //   { 
+    //     to: "0x4EFA4f689d11d94DfF747Cd3dc2cC92717a91055",
+    //     value: "000",
+    //     data: "000",
+    //     chain: "mumbai",
+    //     publicKey: "0x049dda3ebdea43fcc333e067854de5f0a9c93a7d5fb840455fd738b639de4d81fa839ff913da97fa3283bdf716d2e277e00608df497eab8650ef3a71ceec330eff",
+    //     gasPrice: "000",
+    //     gasLimit: "000",
+    //   }
+    //   )
+    // console.log(auth)
+
+    // return;
 
 
     const provider = new providers.StaticJsonRpcProvider('https://rpc-mumbai.maticvigil.com/');
