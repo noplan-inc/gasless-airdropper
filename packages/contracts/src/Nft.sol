25135550000424475de44bcf808c7e1fac1fd94a
折田幸夫 <3204704@gmail.com>
2023-02-11 00:24:37 +0900

    feat: nft contract

--- packages/contracts/src/Nft.sol
+++ packages/contracts/src/Nft.sol
@@ -6,16 +6,20 @@ import "openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "./interfaces/INFT.sol";
 
-contract Nft is ERC721, INFT {
+contract Nft is ERC721, ERC721URIStorage, INFT {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
-    constructor()ERC721("NftDemo", "NFT"){}
+    constructor()ERC721("Nft", "NFT"){}
 
     function safeMint(address to, string memory uri) public{
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
