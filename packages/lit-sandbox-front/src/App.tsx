1e16d435c74193125477c5ca382e6057449ac4af
somasekimoto <0421.soma@gmail.com>
2023-02-11 13:53:36 +0900

    create: mint directly

--- packages/lit-sandbox-front/src/App.tsx
+++ packages/lit-sandbox-front/src/App.tsx
@@ -69,22 +69,18 @@ const ConnectButton: React.FC<{}> = () => {
     await client.connect();
     console.log(client);
 
+    const provider = new providers.StaticJsonRpcProvider('https://rpc-mumbai.maticvigil.com/');
     
-
-    // const provider = new providers.StaticJsonRpcProvider('https://rpc-mumbai.maticvigil.com/');
-    const provider = new providers.Web3Provider((window as any).ethereum)
-
-    //自分のアドレスアカウントがわかる
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    console.log("signer")
-    console.log(signer)
     // NFT配布先アドレス
-    // const metamaskAddress = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199" // soma address
-    const metamaskAddress = signer.getAddress() // soma address
+    const metamaskAddress = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199" // soma address
+    const mintTo = "0x2ad81113A9414A1c4EfA2351fde2accaa6a79FaE"
+
+    // https://explorer.litprotocol.com/profile にてlit.protocolのアカウント作ってpublickey作成する
+    const litPublicKey = "0x" + "049dda3ebdea43fcc333e067854de5f0a9c93a7d5fb840455fd738b639de4d81fa839ff913da97fa3283bdf716d2e277e00608df497eab8650ef3a71ceec330eff"
     // airdrop contract MerkleDistributor
-    const airdropContractAddress = "0x4EFA4f689d11d94DfF747Cd3dc2cC92717a91055"
-    const airdropContract = new Contract(airdropContractAddress, airdropABI, signer);
+    
+    const airdropContractAddress = "0xb1b3d3930eC3A721Db287D21c1ff0541C2Fc5849"
+    const airdropContract = new Contract(airdropContractAddress, airdropABI, provider);
     
     // bytes32[] ウォレットアドレスが、ホワイトリストに入っているかどうか証明するために必要な値たち rootに辿り着くために必要なleafの片割れたち
     const merkleProof = [formatBytes32String("1"), formatBytes32String("2")]
@@ -93,38 +89,56 @@ const ConnectButton: React.FC<{}> = () => {
     // ミントする量
     const mintAmount = 1
     
+    const data = airdropContract.interface.encodeFunctionData("claim", [index, metamaskAddress, mintAmount, merkleProof, "tokenuri"]);
+    // const data = airdropContract.interface.encodeFunctionData("claim", [index, mintTo, mintAmount, merkleProof, "tokenuri"]);
     // const data = airdropContract.interface.encodeFunctionData("claim", [index, "0xc9b1CF19765d4DB31024AdFE09D14603cD56a476", mintAmount, merkleProof]);
-    // const data = airdropContract.interface.encodeFunctionData("claim", [index, metamaskAddress, mintAmount, merkleProof]);
 
-    // const gas = await airdropContract.estimateGas.claim(index, metamaskAddress, mintAmount, merkleProof, {from: metamaskAddress});
-    // const gas = await airdropContract.estimateGas.claim(index, "0xc9b1CF19765d4DB31024AdFE09D14603cD56a476", mintAmount, merkleProof, {from: metamaskAddress});
+    const gas = await airdropContract.estimateGas.claim(index, metamaskAddress, mintAmount, merkleProof, "tokeuri", {from: metamaskAddress});
+    // const gas = await airdropContract.estimateGas.claim(index, mintTo, mintAmount, merkleProof, "tokeuri", {from: metamaskAddress});
+    // const gas = await airdropContract.estimateGas.claim(index, "0xc9b1CF19765d4DB31024AdFE09D14603cD56a476", mintAmount, merkleProof, "tokeuri", {from: metamaskAddress});
 
-    // const result = await airdropContract.claim(index, "0xc9b1CF19765d4DB31024AdFE09D14603cD56a476", mintAmount, merkleProof, "tokenuri")
+  
+    const params = {
+      provider,
+      to: airdropContractAddress, // airdrop contract address
+      value: "0x",
+      data,
+      chain: 'mumbai',
+      gasLimit: gas.toHexString(),
+      publicKey: litPublicKey // soma PKP publicKey
+    };
+
+    const tx = await client.sendPKPTransaction(params);
+    console.log(`tx`);
+    console.log(tx);
+  }, []);
+
+  const mintDirectlyHandler = useCallback(async () => {
+    // const provider = new providers.StaticJsonRpcProvider('https://rpc-mumbai.maticvigil.com/');
+    const provider = new providers.Web3Provider((window as any).ethereum)
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const metamaskAddress = signer.getAddress() // soma address
+    // airdrop contract MerkleDistributor
+    const airdropContractAddress = "0x4EFA4f689d11d94DfF747Cd3dc2cC92717a91055"
+    const airdropContract = new Contract(airdropContractAddress, airdropABI, signer);
+    // bytes32[] ウォレットアドレスが、ホワイトリストに入っているかどうか証明するために必要な値たち rootに辿り着くために必要なleafの片割れたち
+    const merkleProof = [formatBytes32String("1"), formatBytes32String("2")]
+    // ウォレットアドレスがmerkle tree配列のどの位置にあるか
+    const index = 1
+    // ミントする量
+    const mintAmount = 1
+
     const result = await airdropContract.claim(index, metamaskAddress, mintAmount, merkleProof)
     console.log("result")
     console.log(result)
-
-  
-    // const params = {
-    //   provider,
-    //   to: airdropContractAddress, // airdrop contract address
-    //   value: "0x",
-    //   data,
-    //   chain: 'mumbai',
-    //   gasLimit: gas.mul(2).toHexString(),
-    //   publicKey: '0x049dda3ebdea43fcc333e067854de5f0a9c93a7d5fb840455fd738b639de4d81fa839ff913da97fa3283bdf716d2e277e00608df497eab8650ef3a71ceec330eff' // soma PKP publicKey
-    // };
-    
-
-    // const tx = await client.sendPKPTransaction(params);
-    // console.log(`tx`);
-    // console.log(tx);
   }, []);
 
   return (<>
     <button onClick={sendMaticHandler}>send matic from lit</button>
     <button onClick={transferERC20Handler}>send ERC20 from lit</button>
     <button onClick={airdropNftHandler}>claim airdrop NFT from lit</button>
+    <button onClick={mintDirectlyHandler}>claim airdrop NFT directly</button>
   </>);
 }
 
