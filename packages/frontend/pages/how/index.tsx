1ee5ae8159eff49bd082974ce37a42edebcf4a1c
serinuntius <wasab1gj@gmail.com>
2023-02-12 20:26:32 +0900

    add link

--- packages/frontend/pages/how/index.tsx
+++ packages/frontend/pages/how/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { Layout } from "@/components/Layout";
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 const How: NextPage = () => {
@@ -13,7 +13,7 @@ const How: NextPage = () => {
     >
       <Layout>
         <h1>仕組み</h1>
-        <Link href={"https://www.canva.com/design/DAFZ_eLoFgo/ICO3g2xu8Ssr_0Bv0Z38wQ/view?utm_content=DAFZ_eLoFgo&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"} target="_blank">こちらで紹介しています</Link>
+        <Link href={"https://www.canva.com/design/DAFZ_eLoFgo/ICO3g2xu8Ssr_0Bv0Z38wQ/view?utm_content=DAFZ_eLoFgo&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"} target="_blank"><Text textDecoration={"underline"}>こちらで紹介しています</Text></Link>
       </Layout>      
     </Box>
   );
